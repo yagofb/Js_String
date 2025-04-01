25693b234674606d33468da7d2a018b230d6842e
Yago <yagofernandezblandon1@gmail.com>
2025-04-01 09:59:51 +0200

    tercer ejercicio

--- String.js
+++ String.js
@@ -3,3 +3,6 @@ let cadena = "Hola Mundo";
 
 // 2. Muestra por consola la longitud de la cadena.
 console.log("2. Longitud de la cadena:", cadena.length);
+
+// 3. Muestra por consola la cadena con todos sus caracteres en mayúsculas.
+console.log("3. En mayúsculas:", cadena.toUpperCase());
