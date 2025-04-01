ab5afe845c61c2e53c114823e557a11283c38789
Yago <yagofernandezblandon1@gmail.com>
2025-04-01 09:59:28 +0200

    segundo ejercicio

--- String.js
+++ String.js
@@ -1,3 +1,5 @@
 // 1. Declara una variable cuyo nombre sea cadena y tenga el valor "Hola Mundo".
 let cadena = "Hola Mundo";
 
+// 2. Muestra por consola la longitud de la cadena.
+console.log("2. Longitud de la cadena:", cadena.length);
